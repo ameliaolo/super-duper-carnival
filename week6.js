dc3ecc7763c1284f3afe941e40bdd030d6152573
ameliaolo <113817700+ameliaolo@users.noreply.github.com>
2022-09-25 17:47:10 +1000

    Update week6.js

--- week6.js
+++ week6.js
@@ -1,3 +1,23 @@
 const canvas = document.getElementById("slime");
 const context = canvas.getContext("2d");
 context.globalAlpha = 0.5;
+
+const cursor = {
+  x: innerWidth / 2,
+  y: innerHeight / 2,
+};
+
+addEventListener("mousemove", (e) => {
+  cursor.x = e.clientX;
+  cursor.y = e.clientY;
+});
+
+addEventListener(
+  "touchmove",
+  (e) => {
+    e.preventDefault();
+    cursor.x = e.touches[0].clientX;
+    cursor.y = e.touches[0].clientY;
+  },
+  { passive: false }
+);
