6efb22337a7c7ac6ac72856b4a72301eee8538a0
ameliaolo <113817700+ameliaolo@users.noreply.github.com>
2022-09-26 17:34:07 +1000

    Update week5.js

--- week5.js
+++ week5.js
@@ -1,1 +1,31 @@
+onLoad("playAnimation");
 
+// so that the animation plays on load
+
+playAnimation{function myMove() {
+  let id = null;
+  // the null element is place for an empty string meaning nothing (its just there for now)
+  
+  const elem = document.getElementById("animate");
+  
+  // calling the document to reference elements from said document
+  
+  let pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 5);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+// in other words, if the object moves out of frame stop the animation
+      
+    } else {
+      pos++; 
+      elem.style.top = pos + "px"; 
+      elem.style.left = pos + "px"; 
+     
+      /* if the object isnot near the barrier it will move diagonally towards the bottom of the canvas towards the
+      border until it hits it and restarts the cycle */
+    }
+  }
+}
+}
